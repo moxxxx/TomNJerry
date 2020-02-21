e2403dccfe7e675a2d02a19ced818a59ff63259a
Jojo Duan <jojoduan@WINDOWS2.SCS.CARLETON.CA>
2020-02-21 17:27:53 -0500

    modify code in main, remove BFS

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,5 @@ public class Main {
     public static void main(String[] args) {
 	Node node = new Node(null, 2,2,0);
 	System.out.println(node);
-	BFS bfs = new BFS();
-	bfs.production(node);
     }
 }
